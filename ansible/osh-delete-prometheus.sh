f9dc916ec5241bfa950e16bb7bb97e2ab71eeea0
Mooli Tayer <mtayer@redhat.com>
2017-06-25 11:56:46 +0300

    delete from template

--- ansible/osh-delete-prometheus.sh
+++ ansible/osh-delete-prometheus.sh
@@ -1,18 +1,3 @@
-oc delete serviceaccount prometheus
+oc process -f prometheus.yaml -p NAMESPACE=prometheus | oc delete -f -
 
-oc delete clusterrolebinding prometheus-cluster-reader
-
-oc delete service prometheus
-oc delete service alertmanager
-oc delete service pushgateway
-
-oc delete deployment prometheus
-
-oc delete configmap prometheus
-oc delete configmap alertmanager
-
-oc delete route prometheus
-oc delete route alertmanager
-oc delete route pushgateway
-
-oc delete namespace prometheus
+#oc delete namespace prometheus
