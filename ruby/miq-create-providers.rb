a41e6ce9b907947b8247f976a32551286475eacd
Mooli Tayer <mtayer@redhat.com>
2017-07-05 21:59:12 +0300

    manual

--- ruby/miq-create-providers.rb
+++ ruby/miq-create-providers.rb
@@ -5,8 +5,9 @@ ENV['RAILS_ENV'] = ARGV[0] || 'development'
 require "/home/mtayer/dev/manageiq/config/environment"
 
 # ManageIQ::Providers::Hawkular::DatawarehouseManager.destroy_all
-ManageIQ::Providers::Openshift::ContainerManager.destroy_all
-ManageIQ::Providers::Openshift::MonitoringManager.destroy_all
+provider_class=ManageIQ::Providers::Openshift::ContainerManager
+provider_class.destroy_all
+
 
 # provider_class = ManageIQ::Providers::Hawkular::DatawarehouseManager
 
@@ -14,14 +15,14 @@ prov = provider_class.new(
     :name                      => "container",
     :zone                      => Zone.last,
     :connection_configurations => [{:endpoint       => {:role       => :default,
-                                                        :hostname   => ENV['HAWKULAR_HOST'],
+                                                        :hostname   => ENV['OSH_HOST'],
                                                         :port       => 443,
                                                         :verify_ssl => false},
                                     :authentication => {:role     => :default,
-                                                        :auth_key => ENV['HAWKULAR_TOKEN']}},
+                                                        :auth_key => ENV['OSH_TOKEN']}},
                                    {:endpoint       => {:role       => :prometheus_alerts,
                                                         :hostname   => ENV['PROMETHEUS_ALERTS_HOST'],
-                                                        :port       => 9099,
+                                                        :port       => ENV['PROMETHEUS_ALERTS_PORT'],
                                                         :verify_ssl => false},
                                     :authentication => {:role     => :prometheus_alerts,
                                                         :auth_key => ENV['HAWKULAR_TOKEN']}}
