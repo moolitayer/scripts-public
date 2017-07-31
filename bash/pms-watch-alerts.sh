b5a24d1c1d2724fa343edfbc861ac83928f0659b
Mooli Tayer <mtayer@redhat.com>
2017-07-31 08:43:44 +0300

    manual

--- bash/pms-watch-alerts.sh
+++ bash/pms-watch-alerts.sh
@@ -1,1 +1,15 @@
-curl -v -k --include --no-buffer --header "Authorization: Bearer ${OSH_TOKEN}" --header "Connection: Upgrade" --header "Upgrade: websocket" --header "Sec-WebSocket-Version: 13" --header "Sec-WebSocket-Key: SGVsbG8sIHdvcmxkIQ==" "https://${PROMETHEUS_ALERTS_HOST}/topics/alerts"
+curl \
+  --insecure \
+  --include \
+  --no-buffer \
+  --header "Connection: Upgrade" \
+  --header "Upgrade: websocket" \
+  --header "Host: ${PROMETHEUS_ALERTS_HOST}" \
+  --header "Origin: https://${PROMETHEUS_ALERTS_HOST}" \
+  --header "Sec-WebSocket-Version: 13" \
+  --header "Sec-WebSocket-Key: SGVsbG8sIHdvcmxkIQ==" \
+  --header "Authorization: Bearer ${OSH_TOKEN}" \
+  https://${PROMETHEUS_ALERTS_HOST}/topics/alerts
+
+#curl -v -k --include --no-buffer --header "Authorization: Bearer ${OSH_TOKEN}"  --header "Upgrade: websocket" --header "Sec-WebSocket-Version: 13" --header "Sec-WebSocket-Key: SGVsbG8sIHdvcmxkIQ==" "https://${PROMETHEUS_ALERTS_HOST}/topics/alerts"
+
